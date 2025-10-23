fee1b9d24402581ce7a69280195dd143869313ce
Giovani985 <amorim.giovani@escola.pr.gov.br>
2025-10-23 12:36:41 -0300

    salve as alteraoes que eu mexi hoje

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
+        // Encontra o passo (div) que está atualmente visível
+        const atual = document.querySelector('.passo.ativo');
+        
+        // Constrói o ID do próximo passo a partir do atributo 'data-proximo' do botão
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        // Remove a classe 'ativo' do passo atual (esconde)
+        if (atual) {
+            atual.classList.remove('ativo');
+        }
+
+        // Adiciona a classe 'ativo' ao próximo passo (mostra)
+        const proximoElemento = document.getElementById(proximoPasso);
+        if (proximoElemento) {
+            proximoElemento.classList.add('ativo');
+        } else {
+            console.error('Erro: Próximo passo não encontrado com o ID:', proximoPasso);
+        }
     })
 })
